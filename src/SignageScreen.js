2d28092384627dee493024d162ae856ddd8816a0
Oliver Steele <steele@osteele.com>
2017-03-31 20:51:46 -0400

    signage screen uses playlist

--- src/SignageScreen.js
+++ src/SignageScreen.js
@@ -2,32 +2,70 @@ import React, { Component } from 'react';
 import ReactFireMixin from 'reactfire';
 import reactMixin from 'react-mixin';
 import { FirebaseRef } from './FirebaseClient';
+import TimerMixin from 'react-timer-mixin';
 
 export class SignageScreen extends Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
       apps: [],
-      config: {}
+      config: {},
+      playlist: {},
+      index: null,
     }
+    this.nextFrame = null;
   }
 
   componentDidMount() {
     this.bindAsArray(FirebaseRef.child('apps'), 'apps');
+    this.bindAsObject(FirebaseRef.child('playlist'), 'playlist');
     this.bindAsObject(FirebaseRef.child('config'), 'config');
+    this.setInterval(this.tick, 1000);
+  }
+
+  setupFrame() {
+    const state = this.state
+    if (!state.apps.length || !state.playlist.sequence.length) return;
+    if (state.index > state.playlist.sequence.length) {
+      this.setState({index: 0})
+    }
+
+    this.setState({index: state.index})
+    const frame = state.playlist.sequence[state.index]
+    if (frame) {
+      const duration = frame.duration || state.playlist.duration || 60
+      this.nextFrame = new Date().getTime() + duration * 1000;
+    }
+  }
+
+  tick() {
+    const state = this.state
+    if (!state.apps.length || !state.playlist.sequence) return;
+    if ((new Date()).getTime() >= this.nextFrame) {
+      const index = (1 + state.index) % state.playlist.sequence.length;
+      this.setState({index: index})
+      this.setupFrame();
+    }
   }
 
   render() {
-    const config = this.state.config;
-    const index = config.index || 0;
-    const app = this.state.apps[index];
+    const state = this.state
+    if (state.index == null || !state.config.screen) {
+      return <div><h1>Loading…</h1></div>
+    }
+    const config = state.config;
+    if (state.index > state.playlist.sequence.length) {
+      return <div>invalid playlist index: {state.index}</div>
+    }
+    const appIndex = state.playlist.sequence[state.index];
+    const app = state.apps[appIndex.app];
     if (!app) {
-      return (<div><h1>Loading…</h1></div>);
+      return <div>invalid app index: {appIndex.app}</div>
     }
     const height = config.screen.height || '800px';
     const style = {position: "relative", height: height, width: "100%"}
     return (
-      <iframe src={app.url} scrolling="no" frameborder="0" style={style}>
+      <iframe src={app.url} scrolling="no" frameBorder="0" style={style}>
         <p>Your browser does not support iframes.</p>
       </iframe>
     )
@@ -35,3 +73,4 @@ export class SignageScreen extends Component {
 }
 
 reactMixin(SignageScreen.prototype, ReactFireMixin);
+reactMixin(SignageScreen.prototype, TimerMixin);
