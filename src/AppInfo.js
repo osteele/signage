ca9c43c2ffe3b753998901dab9be667f351635c7
Oliver Steele <steele@osteele.com>
2017-04-04 15:40:03 -0400

    the app URL is editable

--- src/AppInfo.js
+++ src/AppInfo.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import ReactFireMixin from 'reactfire';
 import reactMixin from 'react-mixin';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { RIETextArea } from 'riek'
 import { FirebaseRef } from './firebase';
+
+const FirebaseAppsRef = FirebaseRef.child('apps');
 
 export default class AppInfoList extends Component {
   state = {
@@ -10,13 +13,15 @@ export default class AppInfoList extends Component {
   };
 
   componentDidMount() {
-    this.bindAsArray(FirebaseRef.child('apps'), 'apps');
+    this.bindAsArray(FirebaseAppsRef, 'apps');
   }
 
   renderItem(app) {
-    return <ListGroupItem key={app['.key']}>
-      <h3>{app.name}</h3>
-      <a href="{app.url}" target="_"><code>{app.url}</code></a></ListGroupItem>
+    return (
+      <ListGroupItem key={app['.key']}>
+        <AppInfo app={app} />
+      </ListGroupItem>
+    );
   }
 
   render() {
@@ -27,5 +32,32 @@ export default class AppInfoList extends Component {
     )
   }
 }
-
 reactMixin(AppInfoList.prototype, ReactFireMixin);
+
+class AppInfo extends Component {
+  changedState = (state) => {
+    const app = this.props.app;
+    for (let [k, v] of Object.entries(state)) {
+      FirebaseAppsRef.child(app['.key']).child(k).set(v);
+    }
+  }
+
+  isWellFormedURL = (url) => url.match(/^https?:\/\/.+/);
+
+  render() {
+    const app = this.props.app;
+    return (
+      <div>
+        <h3>{app.name}
+          <small> (<a href="{app.url}" target="_">site</a>)</small>
+        </h3>
+        <code><RIETextArea
+          value={app.url}
+          change={this.changedState}
+          propName='url'
+          validate={this.isWellFormedURL}
+          classInvalid="invalid" /></code>
+      </div>
+    );
+  }
+}
